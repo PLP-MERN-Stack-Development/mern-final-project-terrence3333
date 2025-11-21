2088ca96aa97ccbaa3f71ecfa297dab820fda616
Terrence Ng'andu <nganduterrence0@gmail.com>
2025-11-21 15:11:43 +0200

    Update socket.js

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -1,1 +1,27 @@
+let io;
+
+export const initSocket = (serverIo) => {
+  io = serverIo;
+
+  io.on('connection', (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+
+    socket.on('joinProject', (projectId) => {
+      socket.join(projectId);
+    });
+
+    socket.on('taskUpdated', (data) => {
+      io.to(data.projectId).emit('taskUpdated', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
+  });
+};
+
+export const getIo = () => {
+  if (!io) throw new Error('Socket.io not initialized!');
+  return io;
+};
 
